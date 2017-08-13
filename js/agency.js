507f1b191da7db718ca0349b1acfec830a0595a2
Andrew Musicant <Andrewmusicant@yahoo.com>
2017-08-13 17:03:07 -0400

    Added text to several fields

--- js/agency.js
+++ js/agency.js
@@ -19,7 +19,7 @@
     });
 
     // Closes the Responsive Menu on Menu Item Click
-    $('.navbar-collapse ul li a').click(function(){ 
+    $('.navbar-collapse ul li a').click(function(){
             $('.navbar-toggle:visible').click();
     });
 
@@ -28,6 +28,6 @@
         offset: {
             top: 100
         }
-    })
+    });
 
 })(jQuery); // End of use strict
